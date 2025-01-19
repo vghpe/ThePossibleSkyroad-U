2c73ca70d7d39ffb2541939bc798e545d44b4c72
Henrik <hpettersson@netflix.com>
2025-01-19 13:07:45 -0800

    Grayed out items

--- Assets/Scripts/Attributes/ReadOnlyInspectorAttribute.cs
+++ Assets/Scripts/Attributes/ReadOnlyInspectorAttribute.cs
@@ -3,16 +3,17 @@ using UnityEditor;
 
 public class ReadOnlyInspectorAttribute : PropertyAttribute { }
 
-#if UNITY_EDITOR 
+#if UNITY_EDITOR
 
 [CustomPropertyDrawer(typeof(ReadOnlyInspectorAttribute))]
 public class ReadOnlyInspectorDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        GUI.enabled = false;
+        EditorGUI.BeginDisabledGroup(true); // Gray out the field
         EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
+        EditorGUI.EndDisabledGroup();
     }
 }
+
 #endif
